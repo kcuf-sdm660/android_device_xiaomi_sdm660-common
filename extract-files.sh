d1859e2c1db8959ded6383e5ebf9a8e715683d6c
Anand Shekhawat <anandsingh215@yahoo.com>
2023-10-14 13:05:53 +0200

    sdm660-common: patch libwvhidl.so to load libprotobuf-cpp-full-3.9.1.so

--- extract-files.sh
+++ extract-files.sh
@@ -71,6 +71,9 @@ function blob_fixup() {
             "${PATCHELF}" --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "${2}"
             sed -i "s|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g" "${2}"
             ;;
+        vendor/lib64/libwvhidl.so)
+            "${PATCHELF}" --replace-needed libprotobuf-cpp-lite-3.9.1.so libprotobuf-cpp-full-3.9.1.so "${2}"
+            ;;
     esac
 }
 
