66b6789c33955cd89679b8fdc653432f7f6f0036
Anand Shekhawat <anandsingh215@yahoo.com>
2023-12-18 09:30:21 +0000

    sdm660-common: Enable Updatable Apex
    disable compression

--- sdm660.mk
+++ sdm660.mk
@@ -53,6 +53,9 @@ PRODUCT_PACKAGES += \
     AntHalService-Soong \
     com.dsi.ant@1.0.vendor
 
+# Apex
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl:32 \
